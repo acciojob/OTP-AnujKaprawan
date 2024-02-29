364d64c2695df27bcd3662a876f2be0927414a2b
Anuj Kaprawan <134187534+AnujKaprawan@users.noreply.github.com>
2024-02-29 20:15:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,18 +1,24 @@
-//your JS code here. If required.
+// Get all input elements with class 'code'
 const inputs = document.querySelectorAll('.code');
 
+// Add event listeners to each input element
 inputs.forEach((input, index) => {
+  // Add input event listener to move focus to next input
   input.addEventListener('input', (e) => {
-    const value = e.target.value;
-    if (value.length === 1 && index < inputs.length - 1) {
-      inputs[index + 1].focus();
-    } else if (value.length === 0 && index > 0) {
-      inputs[index - 1].focus();
+    // If input value is filled
+    if (input.value.length === 1) {
+      // Move focus to next input if not the last input
+      if (index < inputs.length - 1) {
+        inputs[index + 1].focus();
+      }
     }
   });
 
+  // Add keydown event listener to move focus to previous input on backspace
   input.addEventListener('keydown', (e) => {
-    if (e.key === 'Backspace' && index > 0) {
+    // If backspace key is pressed and input value is empty and not the first input
+    if (e.key === 'Backspace' && input.value.length === 0 && index > 0) {
+      // Move focus to previous input
       inputs[index - 1].focus();
     }
   });
